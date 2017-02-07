a38f300ac43acbebb696917db96ef8c6239c05f9
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-02-07 10:27:42 -0300

    adds Collections Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
+import { CollectionsPage } from '../pages/collections/collections';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    TabsPage,
     HomePage,
-    TabsPage
+    CollectionsPage,
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,10 +19,9 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    TabsPage,
     HomePage,
-    TabsPage
+    CollectionsPage,
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ src/pages/collections/collections.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-collections',
+  templateUrl: 'collections.html'
+})
+export class CollectionsPage {
+  constructor(private navCtrl: NavController) { }
+}
